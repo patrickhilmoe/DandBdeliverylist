4deeeb643ccf108b477f9da06beb478f8aa08c92
Patrick Hilmoe <PatrickH@dewaardandbode.com>
2023-12-23 07:15:15 -0800

    added object versions of Time Savers Reports

---
+++ components/open-order-report.jsx
@@ -0,0 +1,179 @@
+export const OpenOrderReport = 
+    [
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 304,
+            "Order Number": 11314787,
+            "Order Date": 45147,
+            "Customer Number": 98941,
+            "Customer Name": "CASSELLA, EJ",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 3328.08,
+            "Total All Payments": 3328.08
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 312,
+            "Order Number": 11316872,
+            "Order Date": 45162,
+            "Customer Number": 128950,
+            "Customer Name": "COLLINS, MISHA",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 8346.76,
+            "Total All Payments": 8118.33
+        },
+        {
+            "Ship-to City": "FERNDALE",
+            "Salesperson": 329,
+            "Order Number": 11317412,
+            "Order Date": 45171,
+            "Customer Number": 141943,
+            "Customer Name": "LOVE, LISA",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 4221.57,
+            "Total All Payments": 4221.57
+        },
+        {
+            "Ship-to City": "FERNDALE",
+            "Salesperson": 329,
+            "Order Number": 11320648,
+            "Order Date": 45261,
+            "Customer Number": 60916,
+            "Customer Name": "BECK, ALICIA & RICK",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 3653.01,
+            "Total All Payments": 3653.01
+        },
+        {
+            "Ship-to City": "BLAINE",
+            "Salesperson": 335,
+            "Order Number": 11320960,
+            "Order Date": 45271,
+            "Customer Number": 186288,
+            "Customer Name": "TOPOR, JANUSZ",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 978.99,
+            "Total All Payments": 978.99
+        },
+        {
+            "Ship-to City": "FERNDALE",
+            "Salesperson": 301,
+            "Order Number": 11321088,
+            "Order Date": 45274,
+            "Customer Number": 46344,
+            "Customer Name": "BEACON MANOR APARTMENTS",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 602.7,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 301,
+            "Order Number": 11321090,
+            "Order Date": 45274,
+            "Customer Number": 183714,
+            "Customer Name": "CHUCKANUT PM & REALTY INC.",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 1527.5,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 301,
+            "Order Number": 11321092,
+            "Order Date": 45274,
+            "Customer Number": 168358,
+            "Customer Name": "SAMISH COMMONS",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 2130.14,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "FERNDALE",
+            "Salesperson": 301,
+            "Order Number": 11321093,
+            "Order Date": 45274,
+            "Customer Number": 1366,
+            "Customer Name": "WINDERMERE MANAGEMENT",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 820.3,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 301,
+            "Order Number": 11321113,
+            "Order Date": 45274,
+            "Customer Number": 24130,
+            "Customer Name": "BOBBINK, BRADY & SHIRLEY",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 881.12,
+            "Total All Payments": 881.12
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 301,
+            "Order Number": 11321124,
+            "Order Date": 45274,
+            "Customer Number": 60310,
+            "Customer Name": "SHEEHAN, TODD",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 1542.66,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 301,
+            "Order Number": 11321131,
+            "Order Date": 45275,
+            "Customer Number": 44450,
+            "Customer Name": "BELLEAU WOODS APTS",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 657.1,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "FERNDALE",
+            "Salesperson": 331,
+            "Order Number": 6001027466,
+            "Order Date": 45222,
+            "Customer Number": 53653,
+            "Customer Name": "MARMON, STEVE & MICHELLE",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 6157.57,
+            "Total All Payments": 6157.57
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 334,
+            "Order Number": 6001028229,
+            "Order Date": 45258,
+            "Customer Number": 47002,
+            "Customer Name": "MONAHAN, MOLLY",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": -471.88,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BELLINGHAM",
+            "Salesperson": 360,
+            "Order Number": 6001028428,
+            "Order Date": 45274,
+            "Customer Number": 83361,
+            "Customer Name": "EDWARDS, CHRISTOPHER",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 635.33,
+            "Total All Payments": 0
+        },
+        {
+            "Ship-to City": "BURLINGTON",
+            "Salesperson": 332,
+            "Order Number": 6001028432,
+            "Order Date": 45274,
+            "Customer Number": 186376,
+            "Customer Name": "PARKS, MORGAN",
+            "Shipping Date": 45278,
+            "Total Invoice Amt": 1475.72,
+            "Total All Payments": 1475.72
+        }
+    ];
